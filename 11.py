d0104166dc08a7813ad289d2c2cdc39bb8093a33
github_username <gowrihifi@gmail.com>
2021-02-10 10:00:27 +0000

    modify

--- 11.py
+++ 11.py
@@ -1,23 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-
-### 11.py
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -61,10 +41,3 @@ httpd = HTTPServer(server_address,MyHandler)
 print("my webserver is running...")
 
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-
-
-## RESULT:
